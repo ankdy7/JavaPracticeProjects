b04bb519b81cfc8eaa16f2d41e0a869ea106dfe5
Ankur Dey <ankur.dey.008@gmail.com>
2024-11-30 00:16:51 +0530

    update

---
+++ src/test/java/BubbleSort.java
@@ -0,0 +1,17 @@
+class BubbleSort {
+    public static void main(String[] args) {
+        int [] arr = {1,1,3,3,2,5,4,8,6,10,9,7};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        for(int n : arr){
+            System.out.print(n+" ");
+        }
+    }
+}
