e7a4f58d43ae596c78d3a9573b11ce23b6bc9d9c
Ankur Dey <ankur.dey.008@gmail.com>
2024-11-29 22:37:03 +0530

    OccurenceOfEachCharInString

--- src/test/java/OccurenceOfEachCharInString.java
+++ src/test/java/OccurenceOfEachCharInString.java
@@ -1,4 +1,34 @@
 public class OccurenceOfEachCharInString {
+    public static void main(String[] args) {
+        String input = "abbbcadeeaaaacccbbbbeeee";
+        countCharacterOccurrences(input);
+    }
 
+    public static void countCharacterOccurrences(String str) {
+        int length = str.length();
+        boolean[] counted = new boolean[length];
 
+        for (int i = 0; i < length; i++) {
+            // Skip if the character has already been counted
+            if (counted[i]) {
+                continue;
+            }
+
+            char currentChar = str.charAt(i);
+            int count = 0;
+
+            // Count occurrences of currentChar
+            for (int j = 0; j < length; j++) {
+                if (str.charAt(j) == currentChar) {
+                    count++;
+                    counted[j] = true; // Mark this character as counted
+                }
+            }
+
+            // Output the count if it's greater than 0
+            if (count > 0) {
+                System.out.print(currentChar +""+ count);
+            }
+        }
+    }
 }
