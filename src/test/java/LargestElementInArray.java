39e38188c2671e0f74d3eb59068eb79fd58c8deb
Ankur Dey <ankur.dey.008@gmail.com>
2024-12-13 22:08:33 +0530

    LargestElementInArray

--- src/test/java/LargestElementInArray.java
+++ src/test/java/LargestElementInArray.java
@@ -1,2 +1,17 @@
 public class LargestElementInArray {
+    public static void main(String[] args) {
+        int [] arr = {1,1,3,3,2,5,4,8,6,10,9,7};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+
+            System.out.print(arr[arr.length-1]);
+
+    }
 }
